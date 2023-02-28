0decb9bd2c030febc1e8b5910a347761a3378513
Romy <romybatista258@gmail.com>
2023-02-27 22:48:54 -0500

    Finished adding CSS
    I finished up this project and look forward to starting my next one.

--- script.js
+++ script.js
@@ -67,13 +67,38 @@ function updatebar(ele) {
             displayWeather: function(data){
               const { name } = data;
               const { icon, description } = data.weather[0];
-              const { temp, humidity } = data. main;
+              const { temp, humidity } = data.main;
               const { speed } = data.wind;
               document.querySelector(".city").innerText = name;
               document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
               document.querySelector(".description").innerText = description;
-              document.querySelector(".temperature").innerText = temp + "°F";
+              document.querySelector(".temperature").innerText = parseInt(temp) + "°F";
               document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-              document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+              document.querySelector(".wind").innerText = "Wind Speed: " + parseInt(speed) + "km/h";
+              document.querySelector(".weather").classList.remove("loading");
+              document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+            },
+            search: function(){
+              this.fetchWeather(document.querySelector(".search-box").value);
             },
           };
+      document.querySelector(".search-button").addEventListener("click", function(){
+        weather.search();
+      });
+
+      document.querySelector(".search-box").addEventListener("keyup", function(event){
+        if(event.key == "Enter"){
+          weather.search();
+        }
+      });
+
+function addCup(){
+  var cups = document.getElementById("drank").innerText;
+  cups++;
+  document.getElementById("drank").innerText = cups;
+}
+
+const upload = () =>{
+  const progressBar = document.querySelector('.progress-bar');
+  progressBar.setAttribute('id', 'play-animation');
+}
